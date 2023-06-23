02d472d2bfae1af13b4900f0728b74a76a2671b5
Austin de Coup-Crank <decoupca@moodys.com>
2023-06-23 12:11:09 -0500

    Implement Table classes

--- msteams_webhooks/containers.py
+++ msteams_webhooks/containers.py
@@ -234,3 +234,131 @@ class ImageSet(CardContainer):
         if self.image_size:
             payload["imageSize"] = self.image_size
         return payload
+
+
+class TableCell(CardContainer):
+    """Table cell.
+
+    https://adaptivecards.io/explorer/TableCell.html
+    """
+
+    TYPE = "TableCell"
+
+    def __init__(
+        self,
+        items: list[CardElement],
+        *,
+        select_action: Optional[Action] = None,
+        style: Optional[types.ContainerStyleTypes] = None,
+        vertical_content_alignment: Optional[types.VerticalAlignmentTypes] = None,
+        bleed: Optional[bool] = None,
+        background_image: Optional[types.URL] = None,
+        min_height: Optional[str] = None,
+        rtl: Optional[bool] = None,
+    ) -> None:
+        self.items = items
+        self.select_action = select_action
+        self.style = style
+        self.vertical_content_alignment = vertical_content_alignment
+        self.bleed = bleed
+        self.background_image = background_image
+        self.min_height = min_height
+        self.rtl = rtl
+
+    def serialize(self) -> dict:
+        payload = {
+            "type": self.TYPE,
+            "items": [x.serialize() for x in self.items],
+        }
+        if self.select_action:
+            payload["selectAction"] = self.select_action.serialize()
+        if self.style:
+            payload["style"] = self.style
+        if self.vertical_content_alignment:
+            payload["verticalContentAlignment"] = self.vertical_content_alignment
+        if self.bleed is not None:
+            payload["bleed"] = self.bleed
+        if self.background_image:
+            payload["backgroundImage"] = self.background_image
+        if self.min_height:
+            payload["minHeight"] = self.min_height
+        if self.rtl is not None:
+            payload["rtl"] = self.rtl
+        return payload
+
+
+class TableRow(CardContainer):
+    """TableRow."""
+
+    TYPE = "TableRow"
+
+    def __init__(self, cells: list[TableCell]) -> None:
+        self.cells = cells
+
+    def serialize(self) -> dict:
+        return {"type": self.TYPE, "cells": [x.serialize() for x in self.cells]}
+
+
+class TableColumn:
+    """TableColumn.
+
+    This entity is not documented beyond a few Table examples. Other properties
+    are unknown.
+    """
+
+    def __init__(self, width: int) -> None:
+        self.width = width
+
+    def serialize(self) -> dict:
+        return {"width": self.width}
+
+
+class Table(CardContainer):
+    """Table.
+
+    https://adaptivecards.io/explorer/Table.html
+    """
+
+    TYPE = "Table"
+
+    def __init__(
+        self,
+        columns: Optional[list[TableColumn]] = None,
+        rows: Optional[list[TableRow]] = None,
+        first_row_as_headers: Optional[bool] = None,
+        show_grid_lines: Optional[bool] = None,
+        grid_style: Optional[types.ContainerStyleTypes] = None,
+        horizontal_cell_content_alignment: Optional[
+            types.HorizontalAlignmentTypes
+        ] = None,
+        vertical_cell_content_alignment: Optional[types.VerticalAlignmentTypes] = None,
+    ) -> None:
+        self.columns = columns
+        self.rows = rows
+        self.first_row_as_headers = first_row_as_headers
+        self.show_grid_lines = show_grid_lines
+        self.grid_style = grid_style
+        self.horizontal_cell_content_alignment = horizontal_cell_content_alignment
+        self.vertical_cell_content_alignment = vertical_cell_content_alignment
+
+    def serialize(self) -> dict:
+        payload = {"type": self.TYPE}
+        if self.columns:
+            payload["columns"] = [x.serialize() for x in self.columns]
+        if self.rows:
+            payload["rows"] = [x.serialize() for x in self.rows]
+        if self.first_row_as_headers is not None:
+            payload["firstRowAsHeaders"] = self.first_row_as_headers
+        if self.show_grid_lines is not None:
+            payload["showGridLines"] = self.show_grid_lines
+        if self.grid_style:
+            payload["gridStyle"] = self.grid_style
+        if self.horizontal_cell_content_alignment:
+            payload[
+                "horizontalCellContentAlignment"
+            ] = self.horizontal_cell_content_alignment
+        if self.vertical_cell_content_alignment:
+            payload[
+                "verticalCellContentAlignment"
+            ] = self.vertical_cell_content_alignment
+        return payload
