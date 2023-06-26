13d31b21ff6a86b7299c7b161d11ebb33a793329
Austin de Coup-Crank <decoupca@moodys.com>
2023-06-26 14:50:28 -0500

    Column unit test.

--- tests/test_containers.py
+++ tests/test_containers.py
@@ -43,3 +43,56 @@ def test_container() -> None:
         rtl=True,
     )
     assert container.serialize() == payload
+
+
+def test_column() -> None:
+    payload = {
+        "type": "ColumnSet",
+        "columns": [
+            {
+                "type": "Column",
+                "items": [
+                    {"type": "TextBlock", "text": "Column 1"},
+                    {
+                        "type": "Image",
+                        "url": "https://adaptivecards.io/content/cats/1.png",
+                        "altText": "Cat",
+                    },
+                ],
+            },
+            {
+                "type": "Column",
+                "items": [
+                    {"type": "TextBlock", "text": "Column 2"},
+                    {
+                        "type": "Image",
+                        "url": "https://adaptivecards.io/content/cats/1.png",
+                        "altText": "Cat",
+                    },
+                ],
+            },
+            {
+                "type": "Column",
+                "items": [
+                    {"type": "TextBlock", "text": "Column 3"},
+                    {
+                        "type": "Image",
+                        "url": "https://adaptivecards.io/content/cats/1.png",
+                        "altText": "Cat",
+                    },
+                ],
+            },
+        ],
+    }
+    image = elements.Image(url="https://adaptivecards.io/content/cats/1.png", alt_text="Cat")
+    col1_text = elements.TextBlock(text="Column 1")
+    col2_text = elements.TextBlock(text="Column 2")
+    col3_text = elements.TextBlock(text="Column 3")
+    col_set = containers.ColumnSet(
+        columns=[
+            containers.Column(items=[col1_text, image]),
+            containers.Column(items=[col2_text, image]),
+            containers.Column(items=[col3_text, image]),
+        ]
+    )
+    assert col_set.serialize() == payload
