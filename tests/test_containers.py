1c489840aa4b629dfd884af95b00b7303e1d8b60
Austin de Coup-Crank <decoupca@moodys.com>
2023-06-26 12:38:24 -0500

    Containers unit test

--- tests/test_containers.py
+++ tests/test_containers.py
@@ -1,4 +1,5 @@
 from msteams_webhooks import containers
+from msteams_webhooks import elements
 from msteams_webhooks.actions import OpenURLAction
 
 
@@ -12,3 +13,21 @@ def test_action_set() -> None:
     action = OpenURLAction(url="https://example.com/", title="Example action")
     action_set = containers.ActionSet(actions=[action])
     assert action_set.serialize() == payload
+
+
+def test_container() -> None:
+    payload = {
+        "type": "Container",
+        "items": [
+            {"type": "TextBlock", "text": "This is some text"},
+            {
+                "type": "Image",
+                "url": "https://adaptivecards.io/content/cats/1.png",
+                "altText": "Cat",
+            },
+        ],
+    }
+    text_block = elements.TextBlock(text="This is some text")
+    image = elements.Image(url="https://adaptivecards.io/content/cats/1.png", alt_text="Cat")
+    container = containers.Container(items=[text_block, image])
+    assert container.serialize() == payload
