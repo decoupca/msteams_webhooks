1ed84f18ac8613b158baf9cf3f1ee43afbc1b064
Austin de Coup-Crank <decoupca@moodys.com>
2023-06-27 08:33:58 -0500

    Additional nox config

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,11 @@
 import nox
 
 
+nox.options.error_on_missing_interpreters = False
+nox.options.stop_on_first_error = False
+nox.options.default_venv_backend = "venv"
+
+
 @nox.session(python=["3.9", "3.10", "3.11"])
 def unit_tests(session) -> None:
     session.install(".", "pytest")
