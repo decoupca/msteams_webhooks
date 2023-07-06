ab16114cad0723bf25828604c6f65927a7fcf3f6
Austin de Coup-Crank <decoupca@moodys.com>
2023-07-06 11:18:22 -0500

    Add dump() convenience alias

--- msteams_webhooks/base.py
+++ msteams_webhooks/base.py
@@ -5,6 +5,10 @@ from typing import Any
 class Entity:
     """Base class for all other entities."""
 
+    def dump(self) -> dict[str, Any]:
+        """Convenience alias."""
+        return self.serialize()
+
     def serialize(self) -> dict[str, Any]:
         """Serialize object into data structure."""
         return {}
